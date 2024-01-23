7d4aec364484e21964c508ff2ecea1cd711af58e
MiiKaa3 <m.kasumagic@gmail.com>
2024-01-23 22:07:54 +1000

    c++/p001 done!

---
+++ c++/p_001.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+int main()
+{
+  constexpr uint64_t max = 10000000;
+
+  std::vector<uint64_t> range(max - 1);
+  std::iota(range.begin(), range.end(), 1);
+  
+  std::vector<uint64_t> res(max - 1);
+  std::transform(
+    range.begin(), range.end(),
+    res.begin(),
+    [](int x) { return !(x % 3ULL) || !(x % 5ULL) ? x : 0; }
+  );
+
+  std::cout << std::reduce(res.begin(), res.end()) << std::endl;
+}
